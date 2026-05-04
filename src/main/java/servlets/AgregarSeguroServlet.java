117b7b5ea240ffcd2168744770f8bd52cdd2e0cd
Bernardo Fiaschi <162641863+BernardoFiaschi@users.noreply.github.com>
2026-05-04 20:54:38 -0300

    Add files via upload

---
+++ src/main/java/servlets/AgregarSeguroServlet.java
@@ -0,0 +1,32 @@
+package servlets;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dao.TipoSeguroDAO;
+import dominio.TipoSeguro;
+
+
+public class AgregarSeguroServlet extends HttpServlet 
+{
+    private static final long serialVersionUID = 1L;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        System.out.println(">>> Entré al AgregarSeguroServlet");
+        
+        TipoSeguroDAO dao = new TipoSeguroDAO();
+        ArrayList<TipoSeguro> listaTipos = dao.traerTodos();
+        request.setAttribute("listaTipos", listaTipos);
+        request.getRequestDispatcher("AgregarSeguro.jsp").forward(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        doGet(request, response);
+    }
+}
